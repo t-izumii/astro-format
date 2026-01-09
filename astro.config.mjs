a3a71c2e45f4f54857ad22f614717017807a55b0
t-izumii <120708670+t-izumii@users.noreply.github.com>
2026-01-09 16:48:07 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ export default defineConfig({
       },
     },
     build: {
-      minify: false,
+      minify: true,
       assetsInlineLimit: 0,
       cssCodeSplit: false,
       rollupOptions: {
