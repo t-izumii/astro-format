19359205300116555d4059944cfc87f9eee391b8
t-izumii <120708670+t-izumii@users.noreply.github.com>
2026-02-02 02:18:03 +0900

    refactor: remove static grid items from top page for cleaner layout

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -5,18 +5,6 @@ export default function Pages() {
     <div>
       <h2>TOP</h2>
 
-      <div class="o-grid" style="--_grid-layout: 1fr 10px 1fr;">
-        <div class="o-grid__item">
-          <h3>Item 1</h3>
-        </div>
-        <div class="o-grid__item">
-          <h3>Item 2</h3>
-        </div>
-        <div class="o-grid__item">
-          <h3>Item 3</h3>
-        </div>
-      </div>
-
       <Picture
         img={{
           src: "https://placehold.jp/150x150.png",
