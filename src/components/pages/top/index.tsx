5bb60e84c0f7e2ea0dc97c5a70bc06343c970f4d
t-izumii <120708670+t-izumii@users.noreply.github.com>
2026-02-02 02:17:27 +0900

    feat: add grid layout component and enhance grid styles for improved responsiveness

--- src/components/pages/top/index.tsx
+++ src/components/pages/top/index.tsx
@@ -5,6 +5,18 @@ export default function Pages() {
     <div>
       <h2>TOP</h2>
 
+      <div class="o-grid" style="--_grid-layout: 1fr 10px 1fr;">
+        <div class="o-grid__item">
+          <h3>Item 1</h3>
+        </div>
+        <div class="o-grid__item">
+          <h3>Item 2</h3>
+        </div>
+        <div class="o-grid__item">
+          <h3>Item 3</h3>
+        </div>
+      </div>
+
       <Picture
         img={{
           src: "https://placehold.jp/150x150.png",
@@ -17,7 +29,7 @@ export default function Pages() {
             src: "https://placehold.jp/150x150.png",
             width: 300,
             height: 200,
-          }
+          },
         ]}
         size={{
           width: "400px",
